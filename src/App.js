684858c594179b2079b9d26b79d31a9cf208f9f1
daniel mcnamara <danmac8407@gmail.com>
2020-02-17 15:05:00 -0600

    Added quarter button for stretch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [lionsScore, setLionsScore] = useState(0);
   const [tigersScore, setTigersScore] = useState(0);
+  const [quarter, setQuarter] = useState(0);
 
   return (
     <div className="container">
@@ -20,8 +21,9 @@ function App() {
             <div className="home__score">{lionsScore}</div>
           </div>
           <div className="timer">00:03</div>
+          <div className="quarter__score">{quarter}</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">tigers</h2>
             <div className="away__score">{tigersScore}</div>
           </div>
         </div>
@@ -32,6 +34,9 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button onClick= {() => setLionsScore(lionsScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
           <button onClick = {() => setLionsScore(lionsScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+        </div>
+        <div className="quarterButton">
+          <button onClick = {() => setQuarter(quarter + 1)} className="quarterButton">Quarter</button>
         </div>
         <div className="awayButtons">
           <button onClick= {() => setTigersScore(tigersScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
