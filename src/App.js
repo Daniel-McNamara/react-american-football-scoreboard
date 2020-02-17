e078744b9b674024dbfbe10743ad32b61e15825a
daniel mcnamara <danmac8407@gmail.com>
2020-02-17 17:57:00 -0600

    Quarter button working, wrong position

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [lionsScore, setLionsScore] = useState(0);
   const [tigersScore, setTigersScore] = useState(0);
-  const [quarter, setQuarter] = useState(0);
 
   return (
     <div className="container">
@@ -21,7 +20,7 @@ function App() {
             <div className="home__score">{lionsScore}</div>
           </div>
           <div className="timer">00:03</div>
-          <div className="quarter__score">{quarter}</div>
+         
           <div className="away">
             <h2 className="away__name">tigers</h2>
             <div className="away__score">{tigersScore}</div>
@@ -35,13 +34,11 @@ function App() {
           <button onClick= {() => setLionsScore(lionsScore + 6)} className="homeButtons__touchdown">Home Touchdown</button>
           <button onClick = {() => setLionsScore(lionsScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
-        <div className="quarterButton">
-          <button onClick = {() => setQuarter(quarter + 1)} className="quarterButton">Quarter</button>
-        </div>
         <div className="awayButtons">
           <button onClick= {() => setTigersScore(tigersScore + 6)} className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick= {() => setTigersScore(tigersScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
+        
       </section>
     </div>
   );
